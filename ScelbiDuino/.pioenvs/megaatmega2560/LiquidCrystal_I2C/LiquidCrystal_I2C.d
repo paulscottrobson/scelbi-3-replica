eb7a9d3792a5671a131f47c0a40fef0cea146e65
Paul Robson <paulscottrobson@gmail.com>
2015-09-21 16:58:31 +0100

    ScelbiDuino lives

---
+++ ScelbiDuino/.pioenvs/megaatmega2560/LiquidCrystal_I2C/LiquidCrystal_I2C.d
@@ -0,0 +1,17 @@
+.pioenvs\megaatmega2560\LiquidCrystal_I2C\LiquidCrystal_I2C.o: \
+ .pioenvs\megaatmega2560\LiquidCrystal_I2C\LiquidCrystal_I2C.cpp \
+ .pioenvs\megaatmega2560\LiquidCrystal_I2C\LiquidCrystal_I2C.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/Print.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/WString.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/Printable.h \
+ .pioenvs\megaatmega2560\Wire/Wire.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/Stream.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/Print.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/Arduino.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/binary.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/WCharacter.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/HardwareSerial.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/Stream.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/USBAPI.h \
+ .pioenvs\megaatmega2560\FrameworkArduino/Arduino.h \
+ .pioenvs\megaatmega2560\FrameworkArduinoVariant/pins_arduino.h
---
+++ ScelbiDuino/.pioenvs/megaatmega2560/src/sys_processor.d
@@ -0,0 +1,2 @@
+.pioenvs\megaatmega2560\src\sys_processor.o: src\sys_processor.cpp \
+ src\sys_processor.h src\drivers.h src\__8008opcodes.h
